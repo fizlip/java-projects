48386fdf4c66032d7c2113294778da8f23acb0c7
Filip Zlatoidsky <fillatino@gmail.com>
2018-11-23 22:38:08 +0100

    reformatted directory

--- crypto/Alphabet.java
+++ crypto/Alphabet.java
@@ -1,15 +1,23 @@
-public class Alphabet{
+import java.util.*;
 
+public class Alphabet<E>{
     // Create english alphabet
-    final private String[] letters = {new Letter(0, "A", "a"), new Letter(1, "B", "b"),
-                new Letter(2, "C", "c"), new Letter(3, "D", "d"), new Letter(4, "E", "e"),
-                new Letter(5, "F", "f"), new Letter(6, "G", "g"), new Letter(7, "H", "h"),
-                new Letter(8, "L", "l"), new Letter(9, "M", "m"), new Letter(10, "N", "n"),
-                new Letter(11, "O", "o"), new Letter(12, "P", "p"), new Letter(13, "Q", "q"),
-                new Letter(14, "R", "r"), new Letter(15, "S", "s"), new Letter(16, "T", "t"),
-                new Letter(17, "U", "u"), new Letter(18, "V", "v"), new Letter(19, "X", "x"),
-                new Letter(20, "Y", "y"), new Letter(21, "Z", "z")};
-    // Use a binary search to find the index of a letter in the alphabet.
-    public indexOf(String letter){
+    public Map<E, Integer> letters = new HashMap<E, Integer>();
+
+    public Alphabet(E[] alphabet){
+        for(int  i = 0; i < alphabet.length; i++){
+            letters.put(alphabet[i], i);
+        }
+    }
+
+    // Shift alphabet i steps.
+    public void shift(int i){
+        for(int j = 0; j < letters.size(); j++){
+            if(j+i < 0){
+                j = letters.size() + (j+1)%(letters.size());
+            }
+            E letter = (E) letters.keySet().toArray()[j];
+            letters.replace(letter, (j+i)%(letters.size()));
+        }
     }
 }
--- crypto/CaesarCipher.java
+++ crypto/CaesarCipher.java
@@ -1,6 +1,36 @@
 import java.util.*;
 
 public class CaesarCipher{
+
+    public Character[] alphabet;
+
+    public CaesarCipher(Character[] alphabet){
+        this.alphabet = alphabet;
+    }
+
+    public String scramble(String message, int s){
+        Alphabet x = new Alphabet<Character>(this.alphabet);
+
+        StringBuilder newMsg = new StringBuilder();
+        newMsg.append(message);
+        x.shift(s);
+        for(int i = 0; i < newMsg.length(); i++){
+            Character oldChar = newMsg.charAt(i);
+            if(oldChar.equals(' ')){
+                continue;
+            }
+            Integer newIndex = Integer.parseInt(x.letters.get(oldChar).toString());
+            Character newChar = this.alphabet[newIndex.intValue()];
+            newMsg.setCharAt(i, newChar.charValue());
+        }
+        return newMsg.toString();
+    }
     public static void main(String[] args) {
+        Character[] alphabet = {'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I',
+                            'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T',
+                            'U', 'V', 'W', 'X', 'Y', 'Z'};
+        CaesarCipher cipher = new CaesarCipher(alphabet);  
+        String message = "ILIIP"; 
+        System.out.println(cipher.scramble(message, 3));
     }
 }
