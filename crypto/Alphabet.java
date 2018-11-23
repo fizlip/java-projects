c09892981d708f18f299280ec5577eee6f08bdc4
Filip Zlatoidsky <fillatino@gmail.com>
2018-11-23 20:44:17 +0100

    add alphabet

---
+++ crypto/Alphabet.java
@@ -0,0 +1,15 @@
+public class Alphabet{
+
+    // Create english alphabet
+    final private String[] letters = {new Letter(0, "A", "a"), new Letter(1, "B", "b"),
+                new Letter(2, "C", "c"), new Letter(3, "D", "d"), new Letter(4, "E", "e"),
+                new Letter(5, "F", "f"), new Letter(6, "G", "g"), new Letter(7, "H", "h"),
+                new Letter(8, "L", "l"), new Letter(9, "M", "m"), new Letter(10, "N", "n"),
+                new Letter(11, "O", "o"), new Letter(12, "P", "p"), new Letter(13, "Q", "q"),
+                new Letter(14, "R", "r"), new Letter(15, "S", "s"), new Letter(16, "T", "t"),
+                new Letter(17, "U", "u"), new Letter(18, "V", "v"), new Letter(19, "X", "x"),
+                new Letter(20, "Y", "y"), new Letter(21, "Z", "z")};
+    // Use a binary search to find the index of a letter in the alphabet.
+    public indexOf(String letter){
+    }
+}
