afbde10ba935d45f199b13e5a8d62a2f7d33d166
Filip Zlatoidsky <fillatino@gmail.com>
2018-12-04 19:57:59 +0100

    More efficient way to reverse an array

--- string-manipulation/StringManipulation.java
+++ string-manipulation/StringManipulation.java
@@ -3,17 +3,31 @@ import java.util.Arrays;
 public class StringManipulation{
     public static void main(String[] args){
         String[] a = {"a","b","c"};
-        System.out.println(Arrays.toString(reverse(a)));
+        Integer[] b = {1,2,3,4,5,6,7,8,9,10};
+        a = reverse2(a);
+        System.out.println(Arrays.toString(a));
     }
-    // Reverse a string, returns the given list in the reversed order.
-    public static String[] reverse(String[] a){
-        String[] array = new String[a.length];
-        for(int i = a.length-1; i >= 0; i--){
-            array[a.length-1-i] = a[i];
-        }
-        return array;
-    }
-
     
+    public static <E> E[] reverse(E[] x){
+        int high = x.length-1;
+        int low = 0;
+        while(low < high){
+            E tmp = x[low];
+            x[low] = x[high];
+            x[high] = tmp;
+            low++;
+            high--;
+        }
+        return x;
+    }    
+
+    public static <E> E[] reverse2(E[] x){
+        for(int i = 0; i < x.length/2; i++){
+            E tmp = x[i];
+            x[i] = x[x.length-i-1];
+            x[x.length-i-1] = tmp;
+        }
+        return x;
+    }
 
 }
