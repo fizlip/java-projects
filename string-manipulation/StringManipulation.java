1a55d883a2ed3b1f2591f8fffffb9e5298b13653
Filip Zlatoidsky <fillatino@gmail.com>
2018-12-04 22:43:05 +0100

    added interleave

--- string-manipulation/StringManipulation.java
+++ string-manipulation/StringManipulation.java
@@ -2,12 +2,9 @@ import java.util.Arrays;
 
 public class StringManipulation{
     public static void main(String[] args){
-        String[] a = {"a","b","c"};
-        Integer[] b = {1,2,3,4,5,6,7,8,9,10};
-        a = reverse2(a);
-        System.out.println(Arrays.toString(a));
+        System.out.println(interleave2("alexander", "p"));
     }
-    
+
     public static <E> E[] reverse(E[] x){
         int high = x.length-1;
         int low = 0;
@@ -29,5 +26,58 @@ public class StringManipulation{
         }
         return x;
     }
+    /**
+     * 
+     * Given two strings the function will return an interleaved version
+     * of these two i.e. every other letter will come from one
+     * every other will come from the other string. 
+     * 
+     * This function uses
+     * a StringBuilder to replace characters at with the first string
+     * if the index is even and replace them with the other string if
+     * the index is odd.
+     * 
+     * The append function is assumed to take constant time and the foorloop will
+     * be linear in respect to the minimum lenght string m, the complexity is
+     * then given by O(m). 
+     * 
+     * @param a 1st string
+     * @param b 2nd string
+     * @return interleaved string
+     */
+    public static String interleave(String a, String b){
+        if(a == null || b == null){
+            throw new IllegalArgumentException();
+        }
+        StringBuilder result = new StringBuilder();
+        result.append(a);result.append(b);
+        for(int i = 0; i < Math.min(a.length(), b.length())*2; i++){
+            result.setCharAt(i, ((i%2 == 0) ? a.charAt(i/2) : b.charAt(i/2)));
+        }
 
+        return result.toString();
+    }
+
+    public static String interleave2(String a, String b){
+        if(a == null || b == null){
+            throw new IllegalArgumentException();
+        }
+        StringBuilder result = new StringBuilder();
+        for(int i = 0; i < Math.min(a.length(), b.length()); i++){
+            StringBuilder tmp = new StringBuilder();
+            tmp.append("xx");
+            tmp.setCharAt(0, a.charAt(i));
+            tmp.setCharAt(1,b.charAt(i));
+            result.append(tmp.toString());
+        }
+        if(a.length() > b.length()){
+            result.append(a.substring(b.length()));
+        }
+        if(b.length() > a.length()){
+            result.append(b.substring(a.length()));
+        }
+        return result.toString();
+    }
+
+    // apnantarik
 }
